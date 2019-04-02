045dd7956142cad2810f81be9a553c3cd6a39bcd
Alex Kopen <alexkopen@gmail.com>
2019-04-01 18:56:04 -0600

    multiple cards

--- src/app/dashboard/news-posts/news-posts.component.ts
+++ src/app/dashboard/news-posts/news-posts.component.ts
@@ -7,9 +7,27 @@ import { NewsPost } from '../../shared/models/news-post.model';
   styleUrls: ['./news-posts.component.scss']
 })
 export class NewsPostsComponent implements OnInit {
+  showOld = false;
+
   @Input() newsPosts: NewsPost[];
 
   constructor() {}
 
   ngOnInit() {}
+
+  get newsPostsFiltered(): NewsPost[] {
+    if (this.showOld) {
+      return this.newsPosts;
+    } else {
+      return this.newsPosts.slice(0, 5);
+    }
+  }
+
+  get postsFooterText(): string {
+    return this.showOld ? 'Hide old posts' : 'Show old posts';
+  }
+
+  toggleOld(): void {
+    this.showOld = !this.showOld;
+  }
 }
